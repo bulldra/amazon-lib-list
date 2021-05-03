415329b7100dab53b09240cc3f40692cd1dc5d0b
bulldra <bullda@gmail.com>
2021-05-03 12:58:27 +0900

    README.md / settings変更

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # amazon-lib-list
 
-kindle for mac のライブラリをパースしてAsinリストを作成する
+kindle for mac が生成するKindleSyncMetadataCache.xmlをパースしてASINリストを作成する
+
+参考：[Kindle蔵書一覧を取得する方法 - Qiita コメント](https://qiita.com/taka_hira/items/8a9181c0733de2c9f8ee#comment-55d0067c26a2fcbaa184)
+
+```bash
+% docker-compose build amazon-lib-list
+```
+
+```bash
+% mkdir work
+% cp ${HOME}/Library/Containers/com.amazon.Kindle/Data/Library/Application\ Support/Kindle/Cache/KindleSyncMetadataCache.xml work/KindleSyncMetadataCache.xml
+```
+
+```bash
+% sh run.sh
+```
+
+入出力ファイルを変更する場合には settigs.json を編集
--- src/settings.py
+++ src/settings.py
@@ -6,8 +6,19 @@ import json
 
 def load_json_setting(path):
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    conf = open(path, 'r')
-    return json.load(conf)
+    if os.path.exists(path):
+        conf = open(path, 'r')
+        return json.load(conf)
+    else:
+        return None
+
+def load_text_list(path):
+    result = []
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+    with open(path, 'r') as conf:
+        for line in conf.read().splitlines():
+            result.append(line)
+    return result
 
 settings_dict = load_json_setting('../config/settings.json')
 secrets_dict = load_json_setting('../config/secrets.json')
