ee63d82fb466833510f299ff98b9d7d14a2e41a6
bulldra <bullda@gmail.com>
2021-05-03 21:38:17 +0900

    キャッシュファイル取得処理のスクリプト化

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-mkdir -p .work 2>/dev/null
+mkdir -p work 2>/dev/null
 cp ${HOME}/Library/Containers/com.amazon.Kindle/Data/Library/Application\ Support/Kindle/Cache/KindleSyncMetadataCache.xml work/
 docker-compose run --entrypoint "python /data/src/main.py" amazon-lib-list
